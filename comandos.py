4a045c6fd5b5f1219ee9568d1c758a3a320fa8b9
AlexisLeon24 <alexisleonjardines@gmail.com>
2026-04-19 21:18:24 -0600

    primeros cambios

---
+++ comandos.py
@@ -0,0 +1,13 @@
+print("hello world")
+print("this is the very first line")
+print("i wonder what should I do now?")
+#this is a comment on python. In other programming languajes, you have to use //
+
+#let´s start working with math
+
+numbera = 12
+numberb = 5500
+numberc = 12400
+
+numberd = numbera + numberc + numberb
+print (numberd)
